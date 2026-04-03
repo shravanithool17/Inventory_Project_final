7183bc694a96e71bcbd5f3c631909b41971d03a4
shravanithool17 <shravanithool17@gmail.com>
2026-04-03 12:19:49 +0530

    Fix: Use MONGO_URL environment variable

--- Inventory_project/backend/server.py
+++ Inventory_project/backend/server.py
@@ -16,7 +16,8 @@ ROOT_DIR = Path(__file__).parent
 # load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = "mongodb://localhost:27017"
+# mongo_url = "mongodb://localhost:27017"
+mongo_url = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(mongo_url)
 # db = client[os.environ['DB_NAME']]
 db = client["test"]
